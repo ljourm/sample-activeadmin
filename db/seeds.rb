aad9a75999434481c6a5250613109fceedd5a963
ljourm <ljourm@gmail.com>
2024-04-29 16:37:38 +0900

    chore: seed.rbの最新化

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+return unless Rails.env.development? # development環境以外では実行しない
+return if AdminUser.exists? # seed実行済みなら終了
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+company1 = Company.create!(name: 'Company 1', code: 'code_1')
+company2 = Company.create!(name: 'Company 2', code: 'code_2')
+
+company_user1 = CompanyUser.create!(company: company1, name: 'Company User 1', active: true)
+company_user2 = CompanyUser.create!(company: company2, name: 'Company User 2', active: true)
+
+role1 = Role.create!(company_user: company_user1, role_type: 1)
+role2 = Role.create!(company_user: company_user2, role_type: 2)
